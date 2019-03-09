814580b133d367dc4f2e3748c208da80bbb16741
iNightElf <mshahzadreza@gmail.com>
2019-03-09 10:26:19 +0600

    Updates

---
+++ precache-manifest.6430fc0fe574befa60e44af11aa3265a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c561c2f9573e7d3ad841",
+    "url": "/yt-clone/static/js/runtime~main.f0bb0a4b.js"
+  },
+  {
+    "revision": "98e6f821a412879dd889",
+    "url": "/yt-clone/static/js/main.e5b29db4.chunk.js"
+  },
+  {
+    "revision": "5d21101413ea9baee411",
+    "url": "/yt-clone/static/js/2.589c286c.chunk.js"
+  },
+  {
+    "revision": "98e6f821a412879dd889",
+    "url": "/yt-clone/static/css/main.4d8df145.chunk.css"
+  },
+  {
+    "revision": "3da3381cd33f774fdb37452912cd18a5",
+    "url": "/yt-clone/index.html"
+  }
+];
